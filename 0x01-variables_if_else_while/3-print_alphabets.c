be086a7294a9eb357915b2861d35a5015c02cbb0
Lefentse Mokgaphame <lefentse2014@gmail.com>
2023-06-20 17:30:45 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print letters in lower and upper case
+ *
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	int l;
+
+	l = 'a';
+	while
+		(l <= 'z')
+		{
+			putchar(l);
+			l++;
+		}
+
+	putchar('\n');
+	return (0);
+}
+
