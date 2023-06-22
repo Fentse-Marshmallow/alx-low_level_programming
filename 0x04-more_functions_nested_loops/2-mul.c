ae8878572da9d55ff2b35ff3866530feed8eea97
Lefentse Mokgaphame <lefentse2014@gmail.com>
2023-06-22 15:56:47 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @n: The first number to be checked
+ * @m: The second number to be checked
+ * Return: Result of multiplication of n parameter and m parameter
+ */
+
+int mul(int n, int m)
+{
+return (n * m);
+}
