7bbfde48a7c2e8946f713e3073044c2ab3471e5a
Lefentse Mokgaphame <lefentse2014@gmail.com>
2023-06-22 16:31:48 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 up to 14 x10 times
+ * Return: numbers from 0 to 14 x10 times
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
